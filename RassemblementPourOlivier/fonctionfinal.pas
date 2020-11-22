99e8a0445107aa9fb87a8867653ca0a5c917eeb0
JulienPirat <72934794+JulienPirat@users.noreply.github.com>
2020-11-22 21:22:48 +0100

    Add files via upload

---
+++ RassemblementPourOlivier/fonctionfinal.pas
@@ -0,0 +1,48 @@
+unit FonctionFinal;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, SystemeBatiments, ressources, GestionEcran;
+
+// Procedure finale pour créer un batiments
+procedure CreeBatiments();
+
+// Procedure affiche les ressources et les Batiments
+PROCEDURE InfoJeuResBat();
+
+implementation
+
+// Procedure affiche les ressources et les Batiments
+PROCEDURE InfoJeuResBat();
+BEGIN
+  effacerEcran();
+  dessinerCadreXY(0,0,148,49, simple, 15, 0); //Cadre début
+  InfoRessource(getValEntrepot());  //Affiche le cadre des ressources avec en parametre la capicité de stockage
+  InfoBatiments();  //Affiche le cadre des batiment possédé
+END;
+
+
+// Procedure finale pour créer un batiments
+procedure CreeBatiments();
+BEGIN
+
+WHILE getRetourBat() = FALSE DO BEGIN
+  InfoJeuResBat();  //affiche les ressources et les Batiments
+  PTypeBatiment();  //Procedure qui class les types de batiments
+  IF getRetourBat() = FALSE THEN BEGIN
+    ConstructionsBatiments(); //Procedure de construction de Batiment
+  END
+END;
+
+  InfoJeuResBat();  //affiche les ressources et les Batiments
+
+  WriteLn('Retour Reussi --> Ecran principal');
+
+  setRetourBat(FALSE);  // modifie la valeur de retour a False
+  ReadLn();
+END;
+
+end.
