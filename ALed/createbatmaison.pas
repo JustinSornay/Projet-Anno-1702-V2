5c0cf56be6a3497fdd2d6d2351559c16ddf73eac
JulienPirat <72934794+JulienPirat@users.noreply.github.com>
2020-12-16 09:30:28 +0100

    Add files via upload

---
+++ ALed/createbatmaison.pas
@@ -0,0 +1,158 @@
+unit CreateBatMaison;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, GestionEcran, GestionErreur, ressources, initialisationPrincip;
+
+//Création d'une maison de Colon
+PROCEDURE CreateBatMaisonCol();
+
+//Création d'une maison de Citoyens
+PROCEDURE CreateBatMaisonCit();
+
+   //                 //
+  //  CoutBatiments //
+//                //
+
+//****Cout Maison de Colon****//
+//Renvoie le Cout du Bois d'une Maison de Colon
+FUNCTION GetCoutBoisMaisonCol():INTEGER;
+
+//****Cout Maison de Citoyen****//
+//Renvoie le Cout du Bois d'une Maison de Citoyen
+FUNCTION GetCoutBoisMaisonCit():INTEGER;
+
+//Renvoie le Cout des Outils d'une Maison de Citoyen
+FUNCTION GetCoutOutilsMaisonCit():INTEGER;
+
+//Renvoie le Cout en Maison de Colon d'une Maison de Citoyen
+FUNCTION GetCoutColMaisonCit():INTEGER;
+
+
+implementation
+CONST
+    //                      //
+  //     Maison Col       //
+//                      //
+    CoutBoisMaisonCol = 2;
+
+    //                      //
+  //     Maison Cit       //
+//                      //
+    CoutBoisMaisonCit = 1;
+    CoutOutilsMaisonCit = 1;
+    CoutColMaisonCit = 1;
+
+Var
+    nbBois, nbOutils, nbBat : Integer;
+
+
+    //********Maison Colon**********//
+    nbMaisonCol: INTEGER;
+
+    //********Curseur**********//
+    CurseurRess : coordonnees;
+
+//Création d'une maison de Colon
+  PROCEDURE CreateBatMaisonCol();
+  BEGIN
+  CurseurRess := positionCurseur();
+  CurseurRess.y := CurseurRess.y + 1;
+  deplacerCurseur(CurseurRess);
+    writeln('Combien de Maison pour les Colons voulez-vous construire ?');
+    CurseurRess.y := CurseurRess.y + 1;
+    deplacerCurseur(CurseurRess);
+    Readln(nbBat);
+    nbBois := nbBat * CoutBoisMaisonCol;  //Calcule du nombre de Bois qu'il va falloir utiliser
+
+      IF (ComparRess(getBois(),nbBois)) THEN BEGIN     //Si le joueur a assez de ressources  //Enlevé  OR et nbOutils
+        setBois(getBois() - nbBois);  //Retrait du cout des batiments sur le bois du joueur
+        setMaisonColon(getMaisonColon() + nbBat); //Ajout du nombre de Maisons de Colons
+        CurseurRess.y := CurseurRess.y + 1;
+        deplacerCurseur(CurseurRess);
+        writeln('Vous avez construit : ',nbBat,' Maison(s) pour les Colons !');
+        CurseurRess.y := CurseurRess.y + 1;
+        deplacerCurseur(CurseurRess);
+        WriteLn('(',nbBois,' Bois)');
+        CurseurRess.y := CurseurRess.y + 1;
+        deplacerCurseur(CurseurRess);
+        readln();
+
+      END
+      ELSE BEGIN  //Si le joueur n'a pas assez de Ressources
+      CurseurRess.y := CurseurRess.y + 1;
+      deplacerCurseur(CurseurRess);
+        writeln('Vous avez pas assez de ressources pour ',nbBat,' Maison(s) de Colons!');
+        ReadLn();
+      END;
+  END;
+
+
+
+//Création d'une maison de Citoyens
+  PROCEDURE CreateBatMaisonCit();
+  BEGIN
+  CurseurRess := positionCurseur();
+  CurseurRess.y := CurseurRess.y + 1;
+  deplacerCurseur(CurseurRess);
+    writeln('Combien de Maison pour les Citoyens voulez-vous construire ?');
+    CurseurRess.y := CurseurRess.y + 1;
+    deplacerCurseur(CurseurRess);
+    Readln(nbBat);              //Il faudrait initialiser nbBat dans l'unité
+    nbMaisonCol := nbBat * CoutColMaisonCit;  //Calcule du nombre de maison de colons qu'il va falloir dépenser
+    nbBois := nbBat * CoutBoisMaisonCit;  //Calcule du nombre de Bois qu'il va falloir utiliser
+    nbOutils := nbBat * CoutOutilsMaisonCit;  //Calcule du nombre d'outils qu'il va falloir utiliser
+
+      IF (ComparRess(getMaisonColon(),nbMaisonCol)) and (ComparRess(getBois(),nbBois)) and (ComparRess(getOutils(),nbOutils))THEN BEGIN     //Si le joueur a assez de ressources
+
+        setMaisonColon(getMaisonColon() - nbMaisonCol);  //Retrait du cout des batiments sur le nombre de Maison de colons
+        setBois(getBois() - nbBois);  //Retrait du cout des batiments sur le bois du joueur
+        setOutils(getOutils() - nbOutils);  //Retrait du cout des batiments sur les outils du joueur
+        setMaisonCitoyens(getMaisonCitoyens() + nbBat); //Ajout du nombre de Maisons de Citoyen
+
+        CurseurRess.y := CurseurRess.y + 1;
+        deplacerCurseur(CurseurRess);
+        writeln('Vous avez construit : ',nbBat,' Maison(s) pour les Citoyens !');
+        CurseurRess.y := CurseurRess.y + 1;
+        deplacerCurseur(CurseurRess);
+        writeln('(',nbOutils,' Outils,',nbBois,' Bois,',nbMaisonCol,' Maisons de Colons)');
+        CurseurRess.y := CurseurRess.y + 1;
+        deplacerCurseur(CurseurRess);
+        readln();
+      END
+      ELSE BEGIN  //Si le joueur n'a pas assez de Ressources
+      CurseurRess.y := CurseurRess.y + 1;
+      deplacerCurseur(CurseurRess);
+        writeln('Vous avez pas assez de ressources pour ',nbBat,' Maison(s) de Citoyen!');
+        ReadLn();
+      END;
+  END;
+
+  //****Maison de Colon****/
+  //Renvoie le Cout du Bois d'une Maison de Colon
+  FUNCTION GetCoutBoisMaisonCol():INTEGER;
+  BEGIN
+    GetCoutBoisMaisonCol := CoutBoisMaisonCol;
+  END;
+
+  //****Maison de Citoyen****//
+  //Renvoie le Cout du Bois d'une Maison de Citoyen
+  FUNCTION GetCoutBoisMaisonCit():INTEGER;
+  BEGIN
+    GetCoutBoisMaisonCit := CoutBoisMaisonCit;
+  END;
+
+  //Renvoie le Cout des Outils d'une Maison de Citoyen
+  FUNCTION GetCoutOutilsMaisonCit():INTEGER;
+  BEGIN
+    GetCoutOutilsMaisonCit := CoutOutilsMaisonCit;
+  END;
+  //Renvoie le Cout en Maison de Colon d'une Maison de Citoyen
+  FUNCTION GetCoutColMaisonCit():INTEGER;
+  BEGIN
+    GetCoutColMaisonCit := CoutColMaisonCit;
+  END;
+end.
---
+++ ALed/initialisationprincip.pas
@@ -0,0 +1,384 @@
+unit initialisationPrincip;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+Procedure InisialisationVariableBat();
+
+//Cette procedure modifie la valeur de la variable MaisonColon
+ procedure setMaisonColon(valeur : integer);
+//Cette fonction renvoie la valeur de la variable MaisonColon
+  function getMaisonColon() : Integer;
+
+  //Cette procedure modifie la valeur de la variable MaisonCitoyens
+   procedure setMaisonCitoyens(valeur : integer);
+   //Cette fonction renvoie la valeur de la variable MaisonColon
+    function getMaisonCitoyens() : Integer;
+
+    //Cette fonction renvoie la valeur de la variable CentreVille
+    function getCentreVille() : Integer;
+    //Cette procedure modifie la valeur de la variable CentreVille
+    procedure setCentreVille(valeur : integer);
+
+    //Cette fonction renvoie la valeur de la variable Chapelle
+    function getChapelle() : Integer;
+    //Cette procedure modifie la valeur de la variable Chapelle
+    procedure setChapelle(valeur : integer);
+
+    //Cette fonction renvoie la valeur de la variable CabanePecheur
+    function getCabanePecheur() : Integer;
+    //Cette procedure modifie la valeur de la variable CabanePecheur
+    procedure setCabanePecheur(valeur : integer);
+
+    //Cette fonction renvoie la valeur de la variable CabaneBucheron
+    function getCabaneBucheron() : Integer;
+    //Cette procedure modifie la valeur de la variable CabaneBucheron
+    procedure setCabaneBucheron(valeur : integer);
+
+    //Cette fonction renvoie la valeur de la variable Bergerie
+    function getBergerie() : Integer;
+    //Cette procedure modifie la valeur de la variable Bergerie
+    procedure setBergerie(valeur : integer);
+
+    //Cette fonction renvoie la valeur de la variable AtelierTissus
+     function getAtelierTissus() : Integer;
+     //Cette procedure modifie la valeur de la variable AtelierTissus
+      procedure setAtelierTissus(valeur : integer);
+
+      //Cette fonction renvoie la valeur de la variable nbEntrepot
+      function getEntrepot() : Integer;
+      //Cette procedure modifie la valeur de la variable nbEntrepot
+      procedure setEntrepot(valeur : integer);
+
+      // ******************Colon et Citoyens****************************//
+
+      //Cette procedure modifie la valeur de la variable Colon
+       procedure setColons(valeur : integer);
+       //Cette fonction renvoie la valeur de la variable Colon
+        function getColons() : Integer;
+
+      //Cette procedure modifie la valeur de la variable Citoyens
+       procedure setCitoyens(valeur : integer);
+       //Cette fonction renvoie la valeur de la variable Citoyens
+        function getCitoyens() : Integer;
+
+
+      //     Valeur Entrepot     //
+//Cette fonction renvoie la capacité de stockage
+FUNCTION getValEntrepot(): Integer;
+//Cette procedure modifie la valeur de la variable ValEntrepot
+procedure setValEntrepot(valeur : integer);
+
+
+// ******************Marchand****************************//
+
+ //Cette fonction renvoie le cout du bois du marchand
+  function MarchandCoutBois() : Integer;
+
+//Cette fonction renvoie le cout du poissons du marchand
+ function MarchandCoutpoissons() : Integer;
+
+//Cette fonction renvoie le cout du Outils du marchand
+ function MarchandCoutOutils() : Integer;
+
+//Cette fonction renvoie le cout du Laine du marchand
+  function MarchandCoutLaine() : Integer;
+
+//Cette fonction renvoie le cout du Tissus du marchand
+  function MarchandCoutTissus() : Integer;
+
+
+implementation
+
+var
+  ValEntrepot : INTEGER;
+  MaisonColon : integer;
+  MaisonCitoyens : INTEGER;
+  CentreVille : INTEGER;
+  Chapelle : INTEGER;
+  CabanePecheur : INTEGER;
+  CabaneBucheron: INTEGER;
+  AtelierTissus: INTEGER;
+  Bergerie: INTEGER;
+  nbEntrepot : INTEGER;
+   Colons : INTEGER;
+   Citoyens : INTEGER;
+
+   MCoutBois : INTEGER;
+   MCoutpoissons : INTEGER;
+   MCoutLaines : INTEGER;
+   MCoutTissus : INTEGER;
+   MCoutOutils : INTEGER;
+Procedure InisialisationVariableBat();
+Begin
+//inisialisations à 0 de nos variables
+//  Cout bois marchand   //
+MCoutBois := 5;
+
+//  Cout poissons marchand   //
+MCoutpoissons := 3;
+
+//  Cout Laines marchand   //
+MCoutLaines := 8;
+
+//  Cout Tissus marchand   //
+MCoutTissus := 8;
+
+//  Cout Outils marchand   //
+MCoutOutils := 15;
+
+
+//     ValEntrepot    //
+ValEntrepot := 40;
+
+//     Maison Citoyen    //
+MaisonCitoyens:=0;
+
+ //     Maison Colons   //
+ MaisonColon:=0;
+
+ //     CentreVille   //
+ CentreVille:=0;
+
+ //     Chapelle   //
+Chapelle:=0;
+
+//    CabanePecheur  //
+CabanePecheur:=0;
+
+//    Entrepot  //
+nbEntrepot := 0;
+
+//  Colons  //
+Colons := 0;
+
+//  Citoyens //
+Citoyens := 0;
+
+end;
+    //               //
+  //    Marchand   //
+//               //
+
+//Cette fonction renvoie le cout du bois du marchand
+ function MarchandCoutBois() : Integer;
+ begin
+   MarchandCoutBois := MCoutBois;
+ end;
+
+ //Cette fonction renvoie le cout du poissons du marchand
+  function MarchandCoutpoissons() : Integer;
+  begin
+    MarchandCoutpoissons := MCoutpoissons;
+  end;
+
+ //Cette fonction renvoie le cout du Outils du marchand
+  function MarchandCoutOutils() : Integer;
+  begin
+    MarchandCoutOutils := MCoutOutils;
+  end;
+
+ //Cette fonction renvoie le cout du Laine du marchand
+   function MarchandCoutLaine() : Integer;
+   begin
+     MarchandCoutLaine := MCoutLaines;
+   end;
+
+ //Cette fonction renvoie le cout du Tissus du marchand
+   function MarchandCoutTissus() : Integer;
+   begin
+     MarchandCoutTissus := MCoutTissus;
+   end;
+
+    //                   //
+  //      Colons       //
+//                   //
+
+//Cette procedure modifie la valeur de la variable Colon
+ procedure setColons(valeur : integer);
+ begin
+  Colons := valeur;
+ end;
+
+ //Cette fonction renvoie la valeur de la variable Colon
+  function getColons() : Integer;
+  begin
+   getColons:= Colons;
+  end;
+
+    //                   //
+  //      Citoyen      //
+//                   //
+
+//Cette procedure modifie la valeur de la variable Citoyens
+ procedure setCitoyens(valeur : integer);
+ begin
+  Citoyens := valeur;
+ end;
+
+ //Cette fonction renvoie la valeur de la variable Citoyens
+  function getCitoyens() : Integer;
+  begin
+   getCitoyens := Citoyens;
+  end;
+
+    //                   //
+  //  Maison Citoyens  //
+//                   //
+
+//Cette fonction renvoie la valeur de la variable MaisonColon
+ function getMaisonCitoyens() : Integer;
+ begin
+  getMaisonCitoyens := MaisonCitoyens;
+ end;
+
+//Cette procedure modifie la valeur de la variable MaisonCitoyens
+ procedure setMaisonCitoyens(valeur : integer);
+ begin
+  MaisonCitoyens := valeur;
+ end;
+
+  //                    //
+ //   Maison Colons   //
+//                  //
+
+//Cette fonction renvoie la valeur de la variable MaisonColon
+ function getMaisonColon() : Integer;
+ begin
+  getMaisonColon := MaisonColon;
+ end;
+
+//Cette procedure modifie la valeur de la variable MaisonColon
+ procedure setMaisonColon(valeur : integer);
+ begin
+  MaisonColon := valeur;
+ end;
+
+ //                     //
+//     CentreVille     //
+//                     //
+
+//Cette fonction renvoie la valeur de la variable CentreVille
+function getCentreVille() : Integer;
+begin
+getCentreVille := CentreVille;
+end;
+
+//Cette procedure modifie la valeur de la variable CentreVille
+procedure setCentreVille(valeur : integer);
+begin
+CentreVille := valeur;
+end;
+
+//                     //
+//     Chapelle        //
+//                     //
+
+//Cette fonction renvoie la valeur de la variable Chapelle
+function getChapelle() : Integer;
+begin
+getChapelle := Chapelle;
+end;
+
+//Cette procedure modifie la valeur de la variable Chapelle
+procedure setChapelle(valeur : integer);
+begin
+Chapelle := valeur;
+end;
+
+//                     //
+//     CabanePecheur   //
+//                     //
+
+//Cette fonction renvoie la valeur de la variable CabanePecheur
+function getCabanePecheur() : Integer;
+begin
+getCabanePecheur := CabanePecheur;
+end;
+
+//Cette procedure modifie la valeur de la variable CabanePecheur
+procedure setCabanePecheur(valeur : integer);
+begin
+CabanePecheur := valeur;
+end;
+
+//                   //
+//   CabaneBucheron  //
+//                   //
+
+//Cette fonction renvoie la valeur de la variable CabaneBucheron
+function getCabaneBucheron() : Integer;
+begin
+getCabaneBucheron := CabaneBucheron;
+end;
+
+//Cette procedure modifie la valeur de la variable CabaneBucheron
+procedure setCabaneBucheron(valeur : integer);
+begin
+CabaneBucheron := valeur;
+end;
+
+//                     //
+//     Bergerie        //
+//                     //
+
+//Cette fonction renvoie la valeur de la variable Bergerie
+function getBergerie() : Integer;
+begin
+getBergerie := Bergerie;
+end;
+
+//Cette procedure modifie la valeur de la variable Bergerie
+procedure setBergerie(valeur : integer);
+begin
+  Bergerie := valeur;
+end;
+
+//Cette fonction renvoie la valeur de la variable AtelierTissus
+ function getAtelierTissus() : Integer;
+ begin
+  getAtelierTissus := AtelierTissus;
+ end;
+
+//Cette procedure modifie la valeur de la variable AtelierTissus
+ procedure setAtelierTissus(valeur : integer);
+ begin
+  AtelierTissus := valeur;
+ end;
+
+ //                     //
+//  Quantité Entrepot  //
+//                    //
+
+//Cette fonction renvoie la valeur de la variable nbEntrepot
+function getEntrepot() : Integer;
+ begin
+   getEntrepot := nbEntrepot;
+ end;
+
+//Cette procedure modifie la valeur de la variable nbEntrepot
+procedure setEntrepot(valeur : integer);
+ begin
+   nbEntrepot := valeur;
+ end;
+
+ //                     //
+ //   Valeur Entrepot   //
+ //                     //
+ //Cette fonction renvoie la valeur de la variable ValEntrepot
+ FUNCTION getValEntrepot(): Integer;
+ begin
+   getValEntrepot := ValEntrepot;
+ end;
+
+ //Cette procedure modifie la valeur de la variable ValEntrepot
+ procedure setValEntrepot(valeur : integer);
+ begin
+     ValEntrepot:= valeur;
+ end;
+
+end.
